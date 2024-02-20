21dca5654888e8a902d888e1c542f62ae1bfed90
brechkoio <brechkoio@gmail.com>
2024-02-20 19:21:23 +0200

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  output: 'server'
+  output: 'server',
+  adapter: netlify()
 });
